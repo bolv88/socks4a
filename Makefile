18d1897e032f583c9b14b8700ab82d50f059f04c
yubo <yubo@yubo-ThinkPad-SL.(none)>
2012-11-29 21:47:04 +0800

    can use now

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 #gcc server_udp_thread.c initserver.c generateSock.c -lpthread
 all:
-	gcc client_socks4a.c initserver.c generateSock.c connect_retry.c -o client.ext
+	gcc client_socks4a.c initserver.c generateSock.c connect_retry.c -lpthread -o client.ext
 	gcc client_old.c initserver.c generateSock.c connect_retry.c -o client_old.ext
--- client_socks4a.c
+++ client_socks4a.c
@@ -1,4 +1,5 @@
 #include "apue.h"
+#include <pthread.h>
 #include <netdb.h>
 #include <errno.h>
 #include <syslog.h>
@@ -17,7 +18,7 @@ extern int initserver(int, struct sockaddr *, socklen_t, int);
 extern struct sockaddr_in generateSockAddr(char *ip, int port);
 extern int connect_retry(int, const struct sockaddr *, socklen_t);
 
-void serve(int sockfd)
+void * serve(void * sockfd)
 {
   int clfd;
   FILE *fp;
@@ -38,111 +39,112 @@ void serve(int sockfd)
 
   for(;;) {
 
-  if ((sockfdS = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-    err = errno;
-    printf("socket error");
-    exit(2);
-  }
-  if (connect_retry(sockfdS, (struct sockaddr*)&addr_in, sizeof(struct sockaddr))<0) {
-     err = errno;
-     fprintf(stderr, "can't connect to server:%s\n", strerror(err));
-     exit(1);
-  } else {
-     printf("connected to server \n");
-  }
-  clfd = accept(sockfd, NULL, NULL);
-  //输入
-  pfds[0].fd=clfd;
-  pfds[0].events=POLLIN;
-  pfds[0].revents=0;
-  //输出
-  pfds[1].fd=sockfdS;
-  pfds[1].events=POLLIN;
-  pfds[1].revents=0;
-
-  for (;;) {
-    //clfd = accept(sockfd, NULL, NULL);
-    int pr=poll(pfds,2,60000);
-    if(pr == 0) continue;
-    if(pr <= -1) {
-        continue;
+    printf("[[[[[[[[[[[[[[[[\n");
+    if ((sockfdS = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+      err = errno;
+      printf("socket error");
+      exit(2);
     }
-    
-    if(pfds[0].revents != 0) {
-        if((nr=read(pfds[0].fd,buffer,BUF_SIZE)) <= 0) {
-            if(nr < 0 ) {
-                printf( "sock4aWroker:read_from_client: ");
-            }
-            close(pfds[0].fd);
-            close(pfds[1].fd);
-            break ;
-        }
-        printf("1: %d\n",(short int)(buffer[0]));
-        printf("2: %d\n",(short int)(buffer[1]));
-        printf("len: %d \n", strlen(buffer));
-        //simpleEncode(buffer,nr);
-        if((nw=send(pfds[1].fd,buffer,nr,0)) != nr) {
-            printf( "sock4aWorker:write_to_server: ");
-            close(pfds[0].fd);
-            close(pfds[1].fd);
-            break ;
-        }
+    if (connect_retry(sockfdS, (struct sockaddr*)&addr_in, sizeof(struct sockaddr))<0) {
+       err = errno;
+       fprintf(stderr, "can't connect to server:%s\n", strerror(err));
+       exit(1);
+    } else {
+       printf("connected to server \n");
     }
-    if(pfds[1].revents != 0) {
-        if((nr=read(pfds[1].fd,buffer,BUF_SIZE)) <= 0) {
-            if(nr < 0 ) {
-                printf( "sock4aWorker:read_from_server: ");
-            }
-            close(pfds[0].fd);
-            close(pfds[1].fd);
-            break ;
-        }
-        //simpleDecode(buffer,nr);
-        printf("res 1: %d\n",(short int)(buffer[0]));
-        printf("res 2: %d\n",(short int)(buffer[1]));
-        if((nw=send(pfds[0].fd,buffer,nr, 0)) != nr) {
-            printf( "sock4aWorker:write_to_client: ");
-            close(pfds[0].fd);
-            close(pfds[1].fd);
-            break ;
-        }
-    }
+    printf("===before accept one\n");
+    clfd = accept(*(int *)sockfd, NULL, NULL);
+    printf("===accept one\n");
+    //输入
+    pfds[0].fd=clfd;
+    pfds[0].events=POLLIN;
     pfds[0].revents=0;
+    //输出
+    pfds[1].fd=sockfdS;
+    pfds[1].events=POLLIN;
     pfds[1].revents=0;
-    /*
-    printf("accept  \n");
-    if (clfd<0) {
-      printf("accept error %s %d \n", strerror(errno), sockfd);
-      exit(1);
-    }
-    if ((n = recv(sockfdS, buf, BUFLEN, 0))>0) {
-      printf("receive from server \n");
-      send(clfd, buf, strlen(buf),0);
-    }
-    //打开远程端口， 发送数据
-    if ((n = recv(clfd, buf, BUFLEN, 0))>0) {
-      printf("receive from brower \n");
-      printf("1: %d \n", buf[0]);
-      printf("2: %d \n", buf[1]);
-      printf("len: %d \n", strlen(buf));
-      send(sockfdS, buf, strlen(buf),0);
-      printf("sended to server\n");
 
-    }
-    //从远程端口接收数据， 返回
-    if ((n = recv(sockfdS, buf, BUFLEN, 0))>0) {
-      printf("receive from server \n");
-      send(clfd, buf, strlen(buf),0);
-    }
+    for (;;) {
+      //clfd = accept(sockfd, NULL, NULL);
+      printf("before poll ==\n");
+      int pr=poll(pfds,2,60000);
+      if(pr == 0) continue;
+      if(pr <= -1) {
+          continue;
+      }
+      printf("=====\n");
+      if(pfds[0].revents != 0) {
+          if((nr=read(pfds[0].fd,buffer,BUF_SIZE)) <= 0) {
+              printf( "sock4aWroker:read_from_client: ");
+              close(pfds[0].fd);
+              close(pfds[1].fd);
+              return ;
+          }
+          printf("1: %d\n",(short int)(buffer[0]));
+          printf("2: %d\n",(short int)(buffer[1]));
+          printf("len: %d \n", strlen(buffer));
+          //simpleEncode(buffer,nr);
+          if((nw=send(pfds[1].fd,buffer,nr,0)) != nr) {
+              printf( "sock4aWorker:write_to_server: ");
+              close(pfds[0].fd);
+              close(pfds[1].fd);
+              return ;
+          }
+      }
+      if(pfds[1].revents != 0) {
+          if((nr=read(pfds[1].fd,buffer,BUF_SIZE)) <= 0) {
+              printf( "sock4aWorker:read_from_server: ");
+              close(pfds[0].fd);
+              close(pfds[1].fd);
+              return ;
+          }
+          //simpleDecode(buffer,nr);
+          printf("res 1: %d\n",(short int)(buffer[0]));
+          printf("res 2: %d\n",(short int)(buffer[1]));
+          if((nw=send(pfds[0].fd,buffer,nr, 0)) != nr) {
+              printf( "sock4aWorker:write_to_client: ");
+              close(pfds[0].fd);
+              close(pfds[1].fd);
+              return ;
+          }
+      }
+      pfds[0].revents=0;
+      pfds[1].revents=0;
+      /*
+      printf("accept  \n");
+      if (clfd<0) {
+        printf("accept error %s %d \n", strerror(errno), sockfd);
+        exit(1);
+      }
+      if ((n = recv(sockfdS, buf, BUFLEN, 0))>0) {
+        printf("receive from server \n");
+        send(clfd, buf, strlen(buf),0);
+      }
+      //打开远程端口， 发送数据
+      if ((n = recv(clfd, buf, BUFLEN, 0))>0) {
+        printf("receive from brower \n");
+        printf("1: %d \n", buf[0]);
+        printf("2: %d \n", buf[1]);
+        printf("len: %d \n", strlen(buf));
+        send(sockfdS, buf, strlen(buf),0);
+        printf("sended to server\n");
 
-    //关闭远程端口
-    //
-    */
-  }
+      }
+      //从远程端口接收数据， 返回
+      if ((n = recv(sockfdS, buf, BUFLEN, 0))>0) {
+        printf("receive from server \n");
+        send(clfd, buf, strlen(buf),0);
+      }
+
+      //关闭远程端口
+      //
+      */
+    }
   }
   close(clfd);
-
+  return;
 }
+pthread_t ntid;
 int main(int argc, char *argv[])
 {
   struct addrinfo *ailist, *aip;
@@ -165,9 +167,15 @@ int main(int argc, char *argv[])
     //addrname = inet_ntop(AF_INET, &sinp->sin_addr, abuf, INET_ADDRSTRLEN);
 
     printf("start serve %s\n", "sdf");
-    serve(sockfd);
+    //serve(sockfd);
+
+    int j=0;
+    for (j=0; j<20;++j) {
+      err = pthread_create(&ntid, NULL, serve, &sockfd);
+    }
+    sleep(5000000);
     exit(0);
-  }else{
+  } else {
     printf("no serve\n");
   }
 
